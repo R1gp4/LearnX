f37a4513422dd7b140aa0df22ca8b01fbeb3964c
Roman Korver <Roman@Romans-MacBook-Air.local>
2016-09-06 21:25:12 +0200

    Another one of my earlier tries

--- Problem1test.py
+++ Problem1test.py
@@ -4,3 +4,12 @@ index = s.find(word[:])
 
 print(index)
 
+#another try I did earlier: it returns all the letters in the string 
+
+s = 'boblkjjlksdbobobobjflksjdfowejajajssssss'
+sub = 'bob'
+count = 0
+for sub in s:
+	count += 1
+
+print(count)
