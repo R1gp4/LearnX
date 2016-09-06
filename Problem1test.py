c16004982d226aee5cdc60b737f471170a97a941
Roman Korver <Roman@Romans-MacBook-Air.local>
2016-09-06 21:53:24 +0200

    adding in range()

--- Problem1test.py
+++ Problem1test.py
@@ -4,7 +4,7 @@ index = s.find(word[:])
 
 print(index)
 
-#another try I did earlier: it returns all the letters in the string 
+#something I tried earlier: this returns 0.
 
 s = 'boblkjjlksdbobobobjflksjdfowejajajssssss'
 sub = 'bob'
@@ -13,10 +13,33 @@ for sub in s:
 	count += 1
 
 print(count)
-#prints all characters.... not what I wanted.
+
+#another try I did earlier: it returns all the letters in the string 
 
 count = 0
 for sub in s[0:2]:
 	count += 1
 print(count)
 
+#prints out 2, which is just as weird as it counting all the characters.
+
+count = 0
+
+for word in s:
+	if word == 'b':
+		if  word == 'o':
+			if word == 'b':
+				count += 1
+print(count)
+
+# this gives back zero, probably because word can't be all three characters at once
+
+count = 0
+
+for n in range(3):
+	if n == 'bob':
+		count += 1
+
+print(count)
+
+#I feel like I'm closer here but I'm not gettin the result yet. time for a break time: 21:52
