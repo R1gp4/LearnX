7c6dd4023a0614b8a8a0daa211686a48853b5552
Roman Korver <roman.korver@gmail.com>
2018-12-13 18:22:00 +0100

    update

---
+++ Colt Steele/new_file.py
@@ -0,0 +1,30 @@
+
+
+while condition:
+    pass
+
+for value in variable:
+    pass
+
+assert {key: value for key, value in variable}
+
+try:
+    pass
+except Exception as e:
+    raise
+else:
+    pass
+finally:
+    pass
+
+
+def fname(arg):
+    pass
+
+class ClassName(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+__init__
---
+++ Steven Foote/js/values.js
@@ -0,0 +1,12 @@
+var projectName = 'kittenbook';
+var versionNumber = '0.0.1';
+var currentDate = new Date(); // Create Date object.
+
+	//currentTime wille look like ' 2014-01-25 at 14:45:12'
+
+var currentTime = 	currentDate.getFullYear() + '-' + 	// set year
+			(currentDate.getMonth() +1)+ '-' + 	// set month
+			currentDate.getDate() + ' at ' + 	// set day of the month
+			currentDate.getHours() + ':' + 		// set hours
+			currentDate.getMinutes() + ':' + 	// set minutes
+			currentDate.getSeconds() + ;    // set seconds
