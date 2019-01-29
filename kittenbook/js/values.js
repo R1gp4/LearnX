e6cc0b2b3d498ffd3516aac67dcef56f09a4a459
Roman Korver <roman.korver@gmail.com>
2019-01-29 23:50:23 +0100

    kittenbook

--- kittenbook/js/values.js
+++ kittenbook/js/values.js
@@ -4,9 +4,15 @@ var currentDate = new Date(); // Create Date object.
 
 	//currentTime wille look like ' 2014-01-25 at 14:45:12'
 
-var currentTime = 	currentDate.getFullYear() + '-' + 	// set year
-			(currentDate.getMonth() +1)+ '-' + 	// set month
+var currentTime = currentDate.getFullYear() + '-' + 	// set year
+			(currentDate.getMonth() + 1) + '-' + 	// set month
 			currentDate.getDate() + ' at ' + 	// set day of the month
 			currentDate.getHours() + ':' + 		// set hours
 			currentDate.getMinutes() + ':' + 	// set minutes
 			currentDate.getSeconds();    // set seconds
+
+var kbValues = {
+			projectName: 'Kittelboek',
+			versionNumber: '0.0.2',
+			currentTime: ['just now']
+};
--- kittenbook/release/main.js
+++ kittenbook/release/main.js
@@ -4,14 +4,20 @@ var currentDate = new Date(); // Create Date object.
 
 	//currentTime wille look like ' 2014-01-25 at 14:45:12'
 
-var currentTime = 	currentDate.getFullYear() + '-' + 	// set year
-			(currentDate.getMonth() +1)+ '-' + 	// set month
+var currentTime = currentDate.getFullYear() + '-' + 	// set year
+			(currentDate.getMonth() + 1) + '-' + 	// set month
 			currentDate.getDate() + ' at ' + 	// set day of the month
 			currentDate.getHours() + ':' + 		// set hours
 			currentDate.getMinutes() + ':' + 	// set minutes
 			currentDate.getSeconds();    // set seconds
 
+var kbValues = {
+			projectName: 'Kittelboek',
+			versionNumber: '0.0.2',
+			currentTime: ['just now']
+};
+
 var userName = prompt('Hello, what\'s your name?');
 document.body.innerHTML = '<h1> Hello, ' + userName + '! </h1>' +
-				'<p>' + projectName + ' ' + versionNumber +
-				' accessed on: ' + currentTime + '</p>';
+				'<p>' + kbValues.projectName + ' ' + kbValues.versionNumber +
+				' accessed on: ' + kbValues.currentTime + '</p>';
